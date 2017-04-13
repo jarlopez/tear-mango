67e3ced9bb2fbca0cec212c7dd750307daa83620
Johan Mickos <jmickos@bu.edu>
2017-04-13 17:15:24 +0200

    Add callbacks base, mimicking C project

---
+++ src/main/java/mango/core/ConfigureCallbacks.java
@@ -0,0 +1,43 @@
+package mango.core;
+
+public class ConfigureCallbacks implements CallbackBase {
+    @Override
+    public String getName() {
+        return null;
+    }
+
+    @Override
+    public void display() {
+
+    }
+
+    @Override
+    public void idle() {
+
+    }
+
+    @Override
+    public void keyboard(int keyState, int scanCode) {
+
+    }
+
+    @Override
+    public void init() {
+
+    }
+
+    @Override
+    public void exit() {
+
+    }
+
+    @Override
+    public void mouse(int buttons, int state, int x, int y) {
+
+    }
+
+    @Override
+    public void mouseMotion(int x, int y) {
+
+    }
+}
